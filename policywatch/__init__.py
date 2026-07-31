6dcaa8c7ec9490ee6c59f2f9c0e13f979b2a7133
Zixsa <50504962+Kozphy@users.noreply.github.com>
2026-08-01 02:14:17 +0800

    Export modular policywatch APIs

--- policywatch/__init__.py
+++ policywatch/__init__.py
@@ -1,5 +1,19 @@
 """Core library for VisaPolicyWatch."""
 
+from .detection import assess_change, changed_lines, keyword_matches
+from .evidence import build_evidence, evidence_to_dict
 from .models import ChangeResult, EvidenceRecord, FetchResult
+from .normalization import extract_main_text, normalize_text
 
-__all__ = ["ChangeResult", "EvidenceRecord", "FetchResult"]
+__all__ = [
+    "ChangeResult",
+    "EvidenceRecord",
+    "FetchResult",
+    "assess_change",
+    "build_evidence",
+    "changed_lines",
+    "evidence_to_dict",
+    "extract_main_text",
+    "keyword_matches",
+    "normalize_text",
+]
